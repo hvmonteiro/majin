d46a8e13c274b32c3b3d5eb11eb24c15b676114e
Hugo Monteiro <hugo.monteiro.pt@gmail.com>
2016-04-22 20:50:05 +0100

    Got build script working for Windows based builds. Not yet the windows setup installer, only the ZIP package.

--- build.sh
+++ build.sh
@@ -1,35 +1,46 @@
 #!/bin/bash
 
 
-
-_check_requirements()   {
-   
-   echo "Checking requirements..." 
-   npm -g ls rcedit > /dev/null 2>&1
-   if [ $? -ne 0 ]; then # Not found globaly, let's check localy
-
-       npm ls rcedit > /dev/null 2>&1
-       if [ $? -ne 0 ]; then
-           # Not found, let's try to install it localy 
-           npm install rcedit 
-           if [ $? -ne 0 ]; then
-               echo "Error: Mandatory NodeJS module 'rcedit' was not found installed. Further more,"
-               echo "     an unknown error was found trying to install it. Check logs for further"
-               echo "      information."
-               echo "Exiting ..."
-               exit 1
-           fi
-       fi
-   fi
-}
+SAVED_DIR="$PWD"
+BUILD_DIR="build"
 
 NODE_PATH="${NODE_PATH:=/usr/lib/node_modules}"
 
 APP_NAME="Majin"
+APP_VERSION="0.1"
+APP_BUILD_VERSION="0000"
+APP_DESCRIPTION="Majin - Mobile Browser for the Desktop"
+APP_COPYRIGHT="Copyright (c) 2016, Hugo V. Monteiro"
+
 APP_PLATFORM=""
 APP_ARCH=""
 APP_ICON=""
-IGNORE_LIST="(resources|(.*).zip|build.sh|devel-notes.md|README*|node_modules/dev-dependency)"
+IGNORE_LIST="(resources|(.*).zip|build.sh|devel-notes.md|README*|node_modules/dev-dependency|build)"
+
+BUILD_DEPENDENCIES="electron-prebuilt"
+
+_my_exit() {
+
+    local EXIT_CODE
+    
+    [ "$1" != "" ] && EXIT_CODE="$1" || EXIT_CODE="0"
+
+    # Change to saved directory (with pushd) before we exit
+    cd "$SAVED_DIR"
+
+    exit "$EXIT_CODE"
+}
+
+_cleanup()  {
+
+    [ -d "${BUILD_DIR}/${APP_NAME}-${APP_PLATFORM}-${APP_ARCH}" ] && rm -rf "${BUILD_DIR}/${APP_NAME}-${APP_PLATFORM}-${APP_ARCH}"
+    rm -f "${BUILD_DIR}/${APP_NAME}-${APP_PLATFORM}-${APP_ARCH}.zip"
+
+}
+
+# Let's save currect directory.
+pushd . > /dev/null
+
 
 case "$1" in
 
@@ -37,11 +48,31 @@ case "$1" in
             APP_PLATFORM="win32"
             APP_ARCH="ia32"
             APP_ICON="images/icon@3.png"
+            EXTRA_PARAMS="--version-string.CompanyName='Hugo Monteiro' \
+--version-string.ProductName='$APP_NAME' \
+--version-string.OriginalFilename='${APP_NAME}.exe' \
+--version-string.ProductName='$APP_NAME' \
+--version-string.InternalName='$APP_NAME' \
+--app-FileDescription='$APP_DESCRIPTION' \
+--app-copyright='$APP_COPYRIGHT' \
+--app-version='$APP_VERSION' \
+--build-version='$APP_BUILD_VERSION' \
+"
             ;;
         win64)
             APP_PLATFORM="win32"
             APP_ARCH="x64"
             APP_ICON="images/icon@3.png"
+            EXTRA_PARAMS="--version-string.CompanyName='Hugo Monteiro' \
+--version-string.ProductName='$APP_NAME' \
+--version-string.OriginalFilename='${APP_NAME}.exe' \
+--version-string.ProductName='$APP_NAME' \
+--version-string.InternalName='$APP_NAME' \
+--app-FileDescription='$APP_DESCRIPTION' \
+--app-copyright='$APP_COPYRIGHT' \
+--app-version='$APP_VERSION' \
+--build-version='$APP_BUILD_VERSION' \
+"
             ;;
         linux32)
             APP_PLATFORM="linux"
@@ -56,7 +87,7 @@ case "$1" in
         osx)
             APP_PLATFORM="darwin"
             APP_ARCH="x64"
-            APP_ICON=""
+            APP_ICON="images/icon@3.hqx"
             ;;
         all)
             for build in win32 win64 linux32 linux64 osx; do
@@ -67,23 +98,40 @@ case "$1" in
             echo ""
             echo " usage: ${0##*/} [win32|win64|linux32|linux64|osx|all]"
             echo ""
-            exit 2
+            _my_exit 2
             ;;
 esac
 
-[ -d "${APP_NAME}-${APP_PLATFORM}-${APP_ARCH}" ] && rm -rf "${APP_NAME}-${APP_PLATFORM}-${APP_ARCH}"
-rm -f ${APP_NAME}-${APP_PLATFORM}-${APP_ARCH}.zip
+_cleanup
 
+if [ ! -d "$BUILD_DIR" ]; then
+    echo "Creating build directory: $BUILD_DIR"
+    mkdir -p $BUILD_DIR 
+    [ $? -ne 0 ] && _my_exit $?
+fi
+
+echo "Installating build dependencies: "
+echo ""
+npm install --save-dev
+echo ""
 echo "Creating package for application named '$APP_NAME'"
+echo ""
 electron-packager . "$APP_NAME" \
 --platform="$APP_PLATFORM" \
 --arch="$APP_ARCH" \
 --icon="$APP_ICON" \
 --ignore="$IGNORE_LIST" \
---overwrite
-[ $? -ne 0 ] && exit 1
+--asar \
+--overwrite \
+--out="$BUILD_DIR" \
+"$EXTRA_PARAMS"
+[ $? -ne 0 ] && _my_exit 1
 
+cd "$BUILD_DIR"
+echo ""
 echo "Creating ZIP package '${APP_NAME}-${APP_PLATFORM}-${APP_ARCH}.zip'"
-zip -q9r "${APP_NAME}-${APP_PLATFORM}-${APP_ARCH}.zip" "${APP_NAME}-${APP_PLATFORM}-${APP_ARCH}"
+zip -qo9r "${APP_NAME}-${APP_PLATFORM}-${APP_ARCH}.zip" "${APP_NAME}-${APP_PLATFORM}-${APP_ARCH}"
 
+cd "$SAVED_DIR"
 
+exit 0
