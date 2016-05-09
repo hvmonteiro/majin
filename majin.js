abc99a4a224a52e21ec61e038ec2428a2e869fe6
Hugo Monteiro <hugo.monteiro.pt@gmail.com>
2016-05-09 14:00:09 +0100

    Updated browsers UserAgent.

--- majin.js
+++ majin.js
@@ -34,7 +34,7 @@ const Tray = electron.Tray;
 const browserOptions = {
   'extraHeaders': 'pragma: no-cache\n',
   // could also be used using webContents.setUserAgent(userAgent)
-  'userAgent': 'Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'
+  'userAgent': 'Mozilla/5.0 (Linux; Android 5.1.1; Nexus 5 Build/LMY48B; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.65 Mobile Safari/537.36'
 };
 
 var vjson = require(path.join(__dirname, 'version.json'));
