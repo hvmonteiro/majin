45897796da7da3917d0d810bf053dde5f0c4e6c0
Hugo Monteiro <hvmonteiro@users.noreply.github.com>
2016-05-14 03:42:26 +0100

    Replaced grunt-html for grunt-html-validation

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,14 +8,22 @@ module.exports = function (grunt) {
       all: ['Gruntfile.js', 'src/majin.js']
     },
     htmllint: { 
-      all: ['src/*.html']
+      options: {
+        reset: grunt.option('reset') || false,
+        stoponerror: false,
+        relaxerror: ['Bad value X-UA-Compatible for attribute http-equiv on element meta.'] //ignores these errors 
+      }
+    },
+    files: {
+        src: ['src/*.html']
     }
+}
   });
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-html');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-htmlhtml-validation');
 
   // Register tasks
   grunt.registerTask('default', ['clean', 'jshint', 'htmllint']);
