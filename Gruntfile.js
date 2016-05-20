44f559a86b4811b42ff4806cc681570d99f6d16e
Hugo Monteiro <hugo.monteiro.pt@gmail.com>
2016-05-20 17:28:14 +0100

    Added alot more options to allow grunt to build App in the future

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,94 @@
 module.exports = function (grunt) {
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     // Before generating any new files, remove any previously-created files.
-    clean: {
+    clean: { 
       build: ['build']
     },
     jshint: {
       all: ['Gruntfile.js', 'src/majin.js']
     },
-    htmllint: { 
+    htmllint: {
       options: {
         reset: grunt.option('reset') || false,
         stoponerror: false,
-        relaxerror: ['Bad value X-UA-Compatible for attribute http-equiv on element meta.'] //ignores these errors 
-      }
-    },
-    files: {
+        relaxerror: ['Bad value X-UA-Compatible for attribute http-equiv on element meta.'] // ignores these errors
+      },
+      files: {
         src: ['src/*.html']
+      }
     },
     copy: {
       main: {
-          files: [
-            // includes files within path
-            { expand: true, src: ['src/images/majin.ico'], dest: 'src/', filter: 'isFile'},
-          ]
+        files: [
+          // includes files within path
+          { expand: true, src: ['src/images/majin.ico'], dest: 'src/', filter: 'isFile' }
+        ]
       }
     },
+    version: {
+      majin: {
+        src: ['package.json', 'src/package.json', 'src/version.json']
+      },
+    },
+
     exec: {
       list_files: {
         cmd: 'electron src/majin.js --test'
+      }
+    },
+    mkdir: {
+      all: {
+        options: {
+          mode: '0700',
+          create: ['build', 'build/target', 'build/packages', 'build/install']
+        }
+      }
+    },
+    electron: {
+      darwin: {
+        options: {
+          dir: 'src',
+          name: 'Majin',
+          platform: 'darwin',
+          arch: 'x64',
+          icon: 'src/images/icon@3.hqx',
+          CompanyName: '',
+          ProductName: 'Majin',
+          OriginalFilename: 'Majin',
+          InternalName: 'Majin',
+          FileDescription: 'Mobile Browser for the Desktop',
+          copyright: 'Copyright (c) 2016, Hugo V. Monteiro',
+          version: '0.1.0',
+          buildVersion: '00',
+          strictSSL: true,
+          ignore: 'node_modules/*',
+          overwrite: true,
+          asar: true,
+          out: 'build/target'
+        }
+      },
+      linux64: {
+        options: {
+          dir: 'src',
+          name: 'Majin',
+          platform: 'linux',
+          arch: 'x64',
+          icon: 'src/images/icon@3.png',
+          CompanyName: 'Hugo V. Monteiro',
+          ProductName: 'Majin',
+          OriginalFilename: 'Majin',
+          InternalName: 'Majin',
+          FileDescription: 'Mobile Browser for the Desktop',
+          copyright: 'Copyright (c) 2016, Hugo V. Monteiro',
+          version: '0.1.0',
+          buildVersion: '00',
+          strictSSL: true,
+          ignore: 'node_modules/*',
+          overwrite: true,
+          asar: true,
+          out: 'build/target'
+        }
       }
     }
   });
@@ -37,8 +98,14 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-exec');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.loadNpmTasks('grunt-version');
+  require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
   // grunt.loadNpmTasks('grunt-html-validation');
 
   // Register tasks
-  grunt.registerTask('default', ['clean', 'copy', 'jshint', 'exec']);
+  grunt.registerTask('delete', ['clean']);
+  grunt.registerTask('default', ['clean', 'copy', 'jshint', 'version', 'exec']);
+  grunt.registerTask('packages', ['clean', 'copy', 'jshint', 'exec', 'mkdir', 'electron']);
+  grunt.registerTask('release', ['clean', 'copy', 'jshint', 'version', 'exec', 'mkdir', 'electron']);
 };
